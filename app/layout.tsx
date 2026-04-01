686fce7d151aba3649ff519d154fd709c428d0b9
Aarnav Koushik <mailtomeask@gmail.com>
2026-03-31 23:01:25 -0700

    contentsquare

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,31 +51,11 @@ export default function RootLayout({
           gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}</Script>
 
-        {/* Meta Pixel */}
-        <Script id="meta-pixel" strategy="afterInteractive">{`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
-          fbq('track', 'PageView');
-        `}</Script>
-
-        {/* Hotjar */}
-        <Script id="hotjar-script" strategy="afterInteractive">{`
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_SITE_ID},hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}</Script>
+        {/* Contentsquare (formerly Hotjar) */}
+        <Script
+          src="https://t.contentsquare.net/uxa/9bf1a263ca280.js"
+          strategy="afterInteractive"
+        />
 
         {/* Segment */}
         <Script id="segment-script" strategy="afterInteractive">{`
