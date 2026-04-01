9be6a8c00c60bdc1e9ad28197cb8efb12a1c1ac6
Aarnav Koushik <mailtomeask@gmail.com>
2026-04-01 13:23:34 -0700

    things_DSFDSF

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,27 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
     clientSegmentCache: true
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'; report-uri https://proxy.csidetm.com/csp; report-to csp-endpoint;"
+          },
+          {
+            key: 'Reporting-Endpoints',
+            value: 'csp-endpoint="https://proxy.csidetm.com/csp"'
+          },
+          {
+            key: 'Report-To',
+            value: '{"group":"csp-endpoint","max_age":10886400,"endpoints":[{"url":"https://proxy.csidetm.com/csp"}]}'
+          }
+        ]
+      }
+    ];
   }
 };
 
